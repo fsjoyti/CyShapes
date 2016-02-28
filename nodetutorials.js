67c825bc509d08dc56d8c77c23e0b36647cef5a6
Fahmida Joyti <fsjoyti@iastate.edu>
2016-02-27 22:32:43 -0600

    file deleted

--- nodetutorials.js
+++ nodetutorials.js
@@ -1,3 +1,7 @@
+/**
+ * Created by Anastasia on 2/19/2016.
+ */
+
 
 var app = require('express')();
 var http = require('http').Server(app);
@@ -5,40 +9,50 @@ var fs = require('fs');
 //var io = require('socket.io')(http);
 var io = require('socket.io');
 
-var sql = require('mysql');
-var querystring = require('querystring');
+
+    var sql = require('mysql');
+    var querystring = require('querystring');
 
 
-var databasecon = sql.createConnection({
-    host:'mysql.cs.iastate.edu',
-    user: 'dbu309grp43',
-    password:'zGtT4R5s5fR',
-    database :'db309grp43'
-});
+    var databasecon = sql.createConnection({
+        host: 'mysql.cs.iastate.edu',
+        user: 'dbu309grp43',
+        password: 'zGtT4R5s5fR',
+        database: 'db309grp43'
+    });
 
 
-databasecon.connect(function(err){
-    if(err){
-        console.log('Error connecting to Db');
-        return;
-    }
-    console.log('Connection established');
-});
-databasecon.query('SELECT * FROM Students',function(err,rows){
-    if(err) throw err;
+    databasecon.connect(function (err) {
+        if (err) {
+            console.log('Error connecting to Db');
+            return;
+        }
+        console.log('Connection established');
+    });
+    databasecon.query('SELECT * FROM Students', function (err, rows) {
+        if (err) throw err;
 
-    console.log('Data received from Db:\n');
-    console.log(rows);
-});
+        console.log('Data received from Db:\n');
+        //console.log(rows);
+        fs.writeFile('table.json', JSON.stringify(rows), function (err) {
+            if (err) throw err;
+            console.log('Saved!');
+        });
+
+
+    });
 
 
 
 app.get('/', function(req, res){
     if (req.method=='GET'&&req.url=='/'){
     res.writeHead(200,{"Content-Type":"text/html"});
+
     fs.createReadStream("./CyShapes.html").pipe(res);
 
+
     }
+
 
 
 
@@ -48,21 +62,36 @@ app.get('/', function(req, res){
 });
 
 
+
+
+
+
+app.locals.jsondata = require('./table.json');
+
+
+
+
 var listener = io.listen(http);
-listener.sockets.on('connection', function(socket){
+listener.sockets.on('connection', function(socket,res) {
     console.log('a user connected');
-    setInterval(function(){
-        socket.emit('date', {'date': new Date()});}, 1000);
+
+
     socket.on('disconnect', function(){
 
         console.log('user disconnected');
 
     });
+
+
     socket.on('client_data', function(data){
         process.stdout.write(data.letter);
         console.log(data.letter);
 
     });
+    socket.on('CH01', function (from, msg) {
+        console.log('MSG', from, ' saying ', msg);
+    });
+
 
 });
 var socket = io.connect;
