c97c4c58b7108771929e0d0f8c4724450194d512
Fahmida Joyti <fsjoyti@iastate.edu>
2016-04-18 14:25:39 -0500

    Building admin panel frontend

--- untitled1/NodeTutorials.js
+++ untitled1/NodeTutorials.js
@@ -37,10 +37,7 @@ app.use(session({secret:'supernova',saveUninitialized:true,resave:true,store:new
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
-
 //app.use('/admin',mongooseadmin());
-
-
 
 
 
